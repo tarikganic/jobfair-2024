1f460c4030f63fd514179d307bcf48f8d7bd0476
tarikganic <riktaganic@gmail.com>
2024-01-20 14:34:07 +0100

    Responsive design and about Best bug fix

--- src/app/about-best/about-best.component.ts
+++ src/app/about-best/about-best.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FaIconComponent,FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-about-best',
   standalone: true,
@@ -10,4 +12,6 @@ import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 })
 export class AboutBestComponent {
   public readonly faInstagram = faInstagram;
+  public readonly faFacebook = faFacebook;
+  public readonly faLinkedin = faLinkedin;
 }
